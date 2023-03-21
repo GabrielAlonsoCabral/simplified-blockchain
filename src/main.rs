3d5cce588224f33ec02ad4b1fcc25e1ef0df1b8c
Gabriel Cabral <gabriel.alonso.dev@icloud.com>
2023-03-21 19:42:45 -0300

    [IMPROVEMENT] -  - master

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,93 @@
-use chrono::{DateTime, Utc};
+use chrono::prelude::*;
+use libp2p::{
+    core::upgrade,
+    futures::StreamExt,
+    mplex,
+    noise::{Keypair, NoiseConfig, X25519Spec},
+    swarm::{Swarm, SwarmBuilder},
+    tcp::TokioTcpConfig,
+    Transport,
+};
 use log::{error, info, warn};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use sha2::{Digest, Sha256};
-
-fn main() {
-    println!("Hello, world!");
-}
+use std::time::Duration;
+use tokio::{
+    io::{stdin, AsyncBufReadExt, BufReader},
+    select, spawn,
+    sync::mpsc,
+    time::sleep,
+};
 
 const DIFFICULTY_PREFIX: &str = "00";
+
+mod p2p;
+
+pub struct App {
+    pub blocks: Vec<Block>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Block {
+    pub id: u64,
+    pub hash: String,
+    pub previous_hash: String,
+    pub timestamp: i64,
+    pub data: String,
+    pub nonce: u64,
+}
+
+impl Block {
+    pub fn new(id: u64, previous_hash: String, data: String) -> Self {
+        let now: DateTime<Utc> = Utc::now();
+        let (nonce, hash) = mine_block(id, now.timestamp(), &previous_hash, &data);
+        Self {
+            id,
+            hash,
+            timestamp: now.timestamp(),
+            previous_hash,
+            data,
+            nonce,
+        }
+    }
+}
+
+fn calculate_hash(id: u64, timestamp: i64, previous_hash: &str, data: &str, nonce: u64) -> Vec<u8> {
+    let data: Value = serde_json::json!({
+        "id": id,
+        "previous_hash": previous_hash,
+        "data": data,
+        "timestamp": timestamp,
+        "nonce": nonce
+    });
+    let mut hasher: Sha256 = Sha256::new();
+    hasher.update(data.to_string().as_bytes());
+    hasher.finalize().as_slice().to_owned()
+}
+
+fn mine_block(id: u64, timestamp: i64, previous_hash: &str, data: &str) -> (u64, String) {
+    info!("mining block...");
+    let mut nonce: u64 = 0;
+
+    loop {
+        if nonce % 100000 == 0 {
+            info!("nonce: {}", nonce);
+        }
+        let hash = calculate_hash(id, timestamp, previous_hash, data, nonce);
+        let binary_hash = hash_to_binary_representation(&hash);
+        if binary_hash.starts_with(DIFFICULTY_PREFIX) {
+            info!(
+                "mined! nonce: {}, hash: {}, binary hash: {}",
+                nonce,
+                hex::encode(&hash),
+                binary_hash
+            );
+            return (nonce, hex::encode(hash));
+        }
+        nonce += 1;
+    }
+}
 
 fn hash_to_binary_representation(hash: &[u8]) -> String {
     let mut res: String = String::default();
@@ -18,11 +97,7 @@ fn hash_to_binary_representation(hash: &[u8]) -> String {
     res
 }
 
-pub struct Blockchain {
-    pub blocks: Vec<Block>,
-}
-
-impl Blockchain {
+impl App {
     fn new() -> Self {
         Self { blocks: vec![] }
     }
@@ -40,7 +115,7 @@ impl Blockchain {
     }
 
     fn try_add_block(&mut self, block: Block) {
-        let latest_block: &Block = self.blocks.last().expect("there is at least one block");
+        let latest_block = self.blocks.last().expect("there is at least one block");
         if self.is_block_valid(&block, latest_block) {
             self.blocks.push(block);
         } else {
@@ -84,8 +159,8 @@ impl Blockchain {
             if i == 0 {
                 continue;
             }
-            let first: &Block = chain.get(i - 1).expect("has to exist");
-            let second: &Block = chain.get(i).expect("has to exist");
+            let first = chain.get(i - 1).expect("has to exist");
+            let second = chain.get(i).expect("has to exist");
             if !self.is_block_valid(second, first) {
                 return false;
             }
@@ -93,9 +168,10 @@ impl Blockchain {
         true
     }
 
+    // We always choose the longest valid chain
     fn choose_chain(&mut self, local: Vec<Block>, remote: Vec<Block>) -> Vec<Block> {
-        let is_local_valid: bool = self.is_chain_valid(&local);
-        let is_remote_valid: bool = self.is_chain_valid(&remote);
+        let is_local_valid = self.is_chain_valid(&local);
+        let is_remote_valid = self.is_chain_valid(&remote);
 
         if is_local_valid && is_remote_valid {
             if local.len() >= remote.len() {
@@ -113,63 +189,102 @@ impl Blockchain {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Block {
-    pub id: u64,
-    pub hash: String,
-    pub previous_hash: String,
-    pub timestamp: i64,
-    pub data: String,
-    pub nonce: u64,
-}
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
 
-impl Block {
-    pub fn new(id: u64, previous_hash: String, data: String) -> Self {
-        let now: DateTime<Utc> = Utc::now();
-        let (nonce, hash) = mine_block(id, now.timestamp(), &previous_hash, &data);
-        Self {
-            id,
-            hash,
-            timestamp: now.timestamp(),
-            previous_hash,
-            data,
-            nonce,
-        }
-    }
-}
+    info!("Peer Id: {}", p2p::PEER_ID.clone());
+    let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
+    let (init_sender, mut init_rcv) = mpsc::unbounded_channel();
 
-fn mine_block(id: u64, timestamp: i64, previous_hash: &str, data: &str) -> (u64, String) {
-    info!("mining block...");
-    let mut nonce: u64 = 0;
+    let auth_keys = Keypair::<X25519Spec>::new()
+        .into_authentic(&p2p::KEYS)
+        .expect("can create auth keys");
+
+    let transp = TokioTcpConfig::new()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
+        .multiplex(mplex::MplexConfig::new())
+        .boxed();
+
+    let behaviour = p2p::AppBehaviour::new(App::new(), response_sender, init_sender.clone()).await;
+
+    let mut swarm = SwarmBuilder::new(transp, behaviour, *p2p::PEER_ID)
+        .executor(Box::new(|fut| {
+            spawn(fut);
+        }))
+        .build();
+
+    let mut stdin = BufReader::new(stdin()).lines();
+
+    Swarm::listen_on(
+        &mut swarm,
+        "/ip4/0.0.0.0/tcp/0"
+            .parse()
+            .expect("can get a local socket"),
+    )
+    .expect("swarm can be started");
+
+    spawn(async move {
+        sleep(Duration::from_secs(1)).await;
+        info!("sending init event");
+        init_sender.send(true).expect("can send init event");
+    });
 
     loop {
-        if nonce % 100000 == 0 {
-            info!("nonce: {}", nonce);
-        }
-        let hash: Vec<u8> = calculate_hash(id, timestamp, previous_hash, data, nonce);
-        let binary_hash: String = hash_to_binary_representation(&hash);
-        if binary_hash.starts_with(DIFFICULTY_PREFIX) {
-            info!(
-                "mined! nonce: {}, hash: {}, binary hash: {}",
-                nonce,
-                hex::encode(&hash),
-                binary_hash
-            );
-            return (nonce, hex::encode(hash));
-        }
-        nonce += 1;
-    }
-}
+        let evt = {
+            select! {
+                line = stdin.next_line() => Some(p2p::EventType::Input(line.expect("can get line").expect("can read line from stdin"))),
+                response = response_rcv.recv() => {
+                    Some(p2p::EventType::LocalChainResponse(response.expect("response exists")))
+                },
+                _init = init_rcv.recv() => {
+                    Some(p2p::EventType::Init)
+                }
+                event = swarm.select_next_some() => {
+                    info!("Unhandled Swarm Event: {:?}", event);
+                    None
+                },
+            }
+        };
 
-fn calculate_hash(id: u64, timestamp: i64, previous_hash: &str, data: &str, nonce: u64) -> Vec<u8> {
-    let data: Value = serde_json::json!({
-        "id": id,
-        "previous_hash": previous_hash,
-        "data": data,
-        "timestamp": timestamp,
-        "nonce": nonce
-    });
-    let mut hasher: Sha256 = sha2::Sha256::new();
-    hasher.update(data.to_string().as_bytes());
-    hasher.finalize().as_slice().to_owned()
+        if let Some(event) = evt {
+            match event {
+                p2p::EventType::Init => {
+                    let peers = p2p::get_list_peers(&swarm);
+                    swarm.behaviour_mut().app.genesis();
+
+                    info!("connected nodes: {}", peers.len());
+                    if !peers.is_empty() {
+                        let req = p2p::LocalChainRequest {
+                            from_peer_id: peers
+                                .iter()
+                                .last()
+                                .expect("at least one peer")
+                                .to_string(),
+                        };
+
+                        let json = serde_json::to_string(&req).expect("can jsonify request");
+                        swarm
+                            .behaviour_mut()
+                            .floodsub
+                            .publish(p2p::CHAIN_TOPIC.clone(), json.as_bytes());
+                    }
+                }
+                p2p::EventType::LocalChainResponse(resp) => {
+                    let json = serde_json::to_string(&resp).expect("can jsonify response");
+                    swarm
+                        .behaviour_mut()
+                        .floodsub
+                        .publish(p2p::CHAIN_TOPIC.clone(), json.as_bytes());
+                }
+                p2p::EventType::Input(line) => match line.as_str() {
+                    "ls p" => p2p::handle_print_peers(&swarm),
+                    cmd if cmd.starts_with("ls c") => p2p::handle_print_chain(&swarm),
+                    cmd if cmd.starts_with("create b") => p2p::handle_create_block(cmd, &mut swarm),
+                    _ => error!("unknown command"),
+                },
+            }
+        }
+    }
 }
